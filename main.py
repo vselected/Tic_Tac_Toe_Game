c55ecbc43157a2a2d6113e69e5942a468ac7ef0b
vselected <dominik.sarleja@gmail.com>
2024-11-19 13:33:02 +0100

    Create functions: 1) Function that takes in the board list object, a marker ('X' or 'O'), and a desired position (number 1-9) and assigns it to the board 2) Function that takes in a board and checks to see if someone has won. 3) Function that uses the random module to randomly decide which player goes first. 4) Function that returns a boolean indicating whether a space on the board is freely available. 5)Function that asks for a player's next position (as a number 1-9) and then uses the function from step 6 to check if its a free position. If it is, then return the position for later use. 6) Function that asks the player if they want to play again and returns a boolean True if they do want to play again.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 
 # Function that will create a board
 def display_board(board):
@@ -21,9 +22,42 @@ def player_input():
     else:
         return ("O", "X")
 
-# Function that will start a game
-def game_start():
-    pass
+# Function that takes in the board list object, a marker ('X' or 'O'), and a desired position (number 1-9) and assigns it to the board
+def place_marker(board, marker, position):
+    board[position] = marker
 
-test_board = ['#','X','O','X','O','X','O','X','O','X']
-display_board(test_board)
+# Function that takes in a board and checks to see if someone has won.
+def win_check(board, mark):
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or  # across the top
+            (board[4] == mark and board[5] == mark and board[6] == mark) or  # across the middle
+            (board[1] == mark and board[2] == mark and board[3] == mark) or  # across the bottom
+            (board[7] == mark and board[4] == mark and board[1] == mark) or  # down the middle
+            (board[8] == mark and board[5] == mark and board[2] == mark) or  # down the middle
+            (board[9] == mark and board[6] == mark and board[3] == mark) or  # down the right side
+            (board[7] == mark and board[5] == mark and board[3] == mark) or  # diagonal
+            (board[9] == mark and board[5] == mark and board[1] == mark))  # diagonal
+
+# Function that uses the random module to randomly decide which player goes first.
+def choose_first():
+    if random.randint(0, 1) == 0:
+        return 'Player 2'
+    else:
+        return 'Player 1'
+
+# Function that returns a boolean indicating whether a space on the board is freely available.
+def space_check(board, position):
+    return board[position] == ' '
+
+# Function that asks for a player's next position (as a number 1-9) and then uses the function from step 6 to check if its a free position.
+# If it is, then return the position for later use.
+def player_choice(board):
+    position = 0
+
+    while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
+        position = int(input('Choose your next position: (1-9) '))
+
+    return position
+
+# Function that asks the player if they want to play again and returns a boolean True if they do want to play again.
+def replay():
+    return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
